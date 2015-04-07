42ebfb69c1dc7413110e932c23a1b68f1cb57cbe
Javier Matas <javier.matas@yump.com>
2015-04-07 18:02:03 +0200

    Arreglos de estructura

--- web/main.dart
+++ web/main.dart
@@ -11,15 +11,17 @@ import 'package:crypto/crypto.dart';
 ButtonElement inButton;
 ButtonElement userButton;
 
-var request;
-var requestDos;
-var serverResponse;
-var tokenid = "";
+var token = "";
+var authUrl = "http://localhost:8080/auth";
+var usersUrl = "http://localhost:8080/admin/user";
+String client;
+String password;
+
 
 void main() {
   initNavMenu();
   inButton = querySelector('#inButton');
-  inButton.onClick.listen(getResponse);
+  inButton.onClick.listen(login);
 
   userButton = querySelector('#userButton');
   userButton.onClick.listen(getUsers);
@@ -38,32 +40,41 @@ void showAbout(RouteEvent e) {
   querySelector('#about').style.display = '';
 }
 
-void showHello(RouteEvent e) {
-  // Extremely simple and non-scalable way to show different views.
-  querySelector('#home').style.display = 'none';
-  querySelector('#about').style.display = '';
-}
-
 void showHome(RouteEvent e) {
   querySelector('#home').style.display = '';
   querySelector('#about').style.display = 'none';
 }
 
-void getResponse(Event e) {
-  var url = "http://localhost:8080/auth?email="+querySelector("#name").value+"&password="+querySelector("#pass").value;
+Function login(Event e) {
 
-  request = new HttpRequest();
-  request.onReadyStateChange.listen(onData);
-  request.open('POST', url);
-  request.send();
-  print(request.responseText);
+  client = querySelector("#name").value;
+  password = querySelector("#pass").value;
+  
+ var data = {
+   'email': client,
+   'password': password
+ };
+
+ HttpRequest.postFormData(authUrl, data).then((HttpRequest req) {
+   print('Request complete ${req.response}');
+
+   Map data = JSON.decode(req.response);
+
+   print('apiId: ${data["apiId"]}');
+   print('apiSecret ${data["apiSecret"]}');
+
+
+   token = window.btoa("${data["apiId"]}:${data["apiSecret"]}");
+   print(token);
+   setUserSession();
+
+ });
 }
 
 void showButtonsForUser(){
-  
   querySelector('#userButton').style.display = '';
-  
 }
+
 void setUserSession(){
     querySelector('#home_login').style.display = 'none';
     querySelector('#home_welcome').style.display = '';
@@ -71,40 +82,7 @@ void setUserSession(){
     showButtonsForUser();
 }
 
-void onData(_) {
-  if (request.readyState == HttpRequest.DONE && request.status == 200) {
-    Map parsedMap = JSON.decode(request.responseText);
-    // Data saved OK.
-    serverResponse = 'Server Sez: ' + request.responseText;
-    var bytes = UTF8.encode(parsedMap["apiId"] + ':' + parsedMap["apiSecret"]);
-    tokenid = CryptoUtils.bytesToBase64(bytes);
-    //var base64= btoa(parsedMap["apiId"] + ':' + parsedMap["apiSecret"]);
-    print("T " + tokenid);
-    setUserSession();
-  } else if (request.readyState == HttpRequest.DONE && request.status == 0) {
-    // Status is 0...most likely the server isn't running.
-    serverResponse = 'No server';
-  }
-}
-
-void userListener(_) {
-  if (requestDos.readyState == HttpRequest.DONE && requestDos.status == 200) {
-    // Data saved OK.
-    serverResponse = 'Server Sez: ' + requestDos.responseText;
-
-    //print(requestDos.responseText);
-  } else if (requestDos.readyState == HttpRequest.DONE && requestDos.status == 0) {
-    // Status is 0...most likely the server isn't running.
-    serverResponse = 'No server';
-  }
-  //Map parsedUsers = JSON.decode(requestDos.responseText);
-  //print("MAP "+parsedUsers[""]);
-  //showUsers(parsedUsers);
-  
-  response(requestDos.responseText);
-}
-
-void response(String name) {
+void draw(String name) {
   querySelector('#resp').text = name;
 }
 
@@ -117,12 +95,16 @@ void showUsers(Map users){
 
 
 void getUsers(Event e) {
-  var url = "http://localhost:8080/admin/user";
+  
+  var basic = "Basic " + token;
+  Map header =  {'Authorization': basic};
+  
+  HttpRequest.request(usersUrl, requestHeaders : header).then((HttpRequest req) {
 
-  var basic = "Basic " + tokenid;
-  requestDos = new HttpRequest();
-  requestDos.onReadyStateChange.listen(userListener);
-  requestDos.open('GET', url);
-  requestDos.setRequestHeader('Authorization', basic);
-  requestDos.send();  
+     //Map data = JSON.decode(req.response);
+
+     draw(req.response);
+
+   });
+
 }
